72bc3287c0fed1f7b110df3f5ac1d6afe9e60b78
kakeru hokida <me@6mile.dev>
2024-01-19 15:29:40 +0900

    WIP: Add stubs

--- core/main.ts
+++ core/main.ts
@@ -6,7 +6,59 @@ function onOpen():void {
       .addToUi();
 }
 
+// モーダルダイアログを表示
 function showModal():void {
   const container: GoogleAppsScript.HTML.HtmlOutput = HtmlService.createHtmlOutputFromFile("webpanel/index.html").setWidth(1200).setHeight(800);
   SpreadsheetApp.getUi().showModalDialog(container, "新入生受付システム");
 }
+
+// ======================================================================================
+// 型定義
+// ======================================================================================
+interface StudentData { // 学生データ
+  studentId: string;
+  studentName: string;
+  pseudonym: string;
+  department: string;
+  remarks: string;
+  dataPosition: {
+    row: number;
+    column: number;
+  };
+}
+
+
+// ======================================================================================
+// 以下がバックエンド処理関数群
+// ======================================================================================
+
+// スプシ上のデータを走査して、該当するデータをWebPanel側に返す関数 (TODO: @sage)
+function getStudentData(studentId: string): StudentData {
+  
+  // 学籍番号のバリデーション
+
+  // 走査処理
+
+  
+  return { 
+    studentId: studentId,
+    studentName: "山田太郎",
+    department: "情報工学科",
+    remarks: "特になし",
+    dataPosition: {
+      row: 2,
+      column: 3
+    }
+  };
+}
+
+// Webパネルから受け取った位置情報の行の色を変更する関数 (TODO: @sage)
+function fillingLine({row, column}: {row: number, column: number}): string {
+  
+  // 位置を特定
+
+  // 塗りつぶし
+
+  return "success";
+}
+
--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import InputBox from '../components/InputBox'
+import { useState } from 'react'
+import StudentIdInputBox from '../components/StudentIdInputBox'
 import Footer from '../components/Footer'
 import UserTable from '../components/UserView'
 
+
 function App() {
+  const [studentId, setStudentId] = useState('')
+
   return (
     <>
       <div className="container py-4">
-        <InputBox />
-        <UserTable />
+        <p>{studentId}</p>
+        <StudentIdInputBox setStudentId={setStudentId} />
+        <UserTable studentId={studentId} />
         <Footer />
       </div>
     </>
